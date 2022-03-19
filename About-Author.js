617f0dd8c7831836285d45f79ad85b7beaa5f686
NotAProgrammer123 <101434656+NotAProgrammer123@users.noreply.github.com>
2022-03-19 02:00:36 +0200

    Update About-Author.js

--- About-Author.js
+++ About-Author.js
@@ -1,4 +1,4 @@
-var socket = io.connect('http://192.168.43.99:8000');
+var socket = io.connect('http://164.92.155.66:8000');
       socket.on("newMessage", (targetUsername) => {
         var cookie = document.cookie;
         var name = cookie.replace("username=", "");
