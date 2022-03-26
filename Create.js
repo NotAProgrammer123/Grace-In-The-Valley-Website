5dfde9c9633a2e8c3143e19704150ca90d322a19
NotAProgrammer123 <101434656+NotAProgrammer123@users.noreply.github.com>
2022-03-26 21:28:40 +0200

    Update Create.js

--- Create.js
+++ Create.js
@@ -1,4 +1,4 @@
-var socket = io.connect('http://164.92.155.66');
+var socket = io.connect('https://gracevalleybook.com');
 function execute () {
 // event.preventDefault();
 var inputUsername = document.getElementById("username").value;
