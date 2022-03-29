cc4bdfbc871967f3b38f0ad6a1e1fbb103e96a47
NotAProgrammer123 <101434656+NotAProgrammer123@users.noreply.github.com>
2022-03-29 02:29:01 +0200

    Update Server.js

--- Server.js
+++ Server.js
@@ -73,7 +73,7 @@ const db = mysql.createConnection({
 
 db.connect((err) => {
   if (err) throw err;
-      db.query("SELECT COUNT(*), email FROM accounts", (err, result) => {
+      db.query("SELECT COUNT(email), email FROM accounts", (err, result) => {
               console.log(result);
         });
 })
